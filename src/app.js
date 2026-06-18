da73bb64a56d3908f1869b7c48efb72aa22b9002
Mohd Sharfuddin Khan <mohdsharfuddinkhan@Mohds-MacBook-Pro.local>
2026-06-18 11:32:00 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require("express")
 
 const app = express()
 
+app.get("/user" , (req , res)=>{
+    res.send("hello from the user")
+})
+
+app.post("/user" , (req, res)=>{
+    res.send("user created")
+})
+
+app.delete("/user" , (req , res) =>{
+    res.send("user deleted")
+})
+
 app.use("/test" , (req , res)=>{
     res.send("hello from test")
 })
